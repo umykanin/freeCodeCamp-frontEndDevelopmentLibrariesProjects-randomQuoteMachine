6c29aa5cb49a51c98e8119b1127cb6558355a87e
umykanin <umykanin@gmail.com>
2022-04-27 22:14:48 +0100

    tests passed

--- random-quote-machine/src/index.js
+++ random-quote-machine/src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>
 )
 
