216a4f13309cffb4a0826b5d74cbc8538852db67
umykanin <umykanin@gmail.com>
2022-04-27 17:54:42 +0100

    working...

--- random-quote-machine/src/app/store.js
+++ random-quote-machine/src/app/store.js
@@ -1,5 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'CREATE_QUOTES': return {
+      ...state,
+      quotes: state.quotes
+    }
+    default:
+      return state
+  }
+}
+
+const actionAxios = 
+
 export const store = configureStore({
-  reducer: {},
+  reducer: reducer,
+  quotes: []
 });
+
